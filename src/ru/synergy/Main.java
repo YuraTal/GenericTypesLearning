27fcc169dba5b81350c203e2d268a862703f0979
YuraTal <jtalabaev@contractcity.ru>
2022-08-23 21:17:39 +0300

    Finished wish instanceof

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Object o = new Scanner(System.in);
+//        Object o = new Scanner(System.in);
+//
+//
+//       Scanner scanner = null;
+//        if (o instanceof Scanner) scanner = (Scanner) o;
+//
+//       if (scanner != null){
+//           scanner.nextInt();
+//       }
+//       // ((String) o ).length ();
+//
+//        Object o1 = new String();
+//
+//        Object o2 = new Integer(15);
 
+        Object[] objects = {10, "Привет", 3.14}; // Здесь происходит автоупаковка
+        for (Object o : objects){
+            if (o instanceof String){
+                String s = (String) o;
+                System.out.println(s);
+            }
+        }
 
-       Scanner scanner = null;
-        if (o instanceof Scanner) scanner = (Scanner) o;
-
-       if (scanner != null){
-           scanner.nextInt();
-       }
-       // ((String) o ).length ();
-
-        Object o1 = new String();
-
-        Object o2 = new Integer(15);
 
 
     }
