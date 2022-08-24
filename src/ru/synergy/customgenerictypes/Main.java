a79814a5f47772ca3afb57ae1dbb5c704a2f167a
YuraTal <jtalabaev@contractcity.ru>
2022-08-24 22:22:41 +0300

    Example of not working Object cast

--- src/ru/synergy/customgenerictypes/Main.java
+++ src/ru/synergy/customgenerictypes/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args){
 
 
+        Account acc1 = new Account(12345 , 5000);
+        System.out.println(acc1.getId());
+
+        Account acc2 = new Account("sid124563", 5000);
+        System.out.println(acc2.getId());
+
+        int a = ((Integer) acc2.getId()) + 10;
+        System.out.println(a);
+
     }
 }
